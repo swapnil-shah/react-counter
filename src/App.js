43cccd1968132b079c400eb51ea28513dbffcd23
Swapnil <62727188+swapnil-shah@users.noreply.github.com>
2021-01-05 12:00:31 -0500

    Added theme context to functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Counter from "./Counter";
 import CounterHooks from "./CounterHooks";
 
+export const ThemeContext = React.createContext();
+
 export default function App() {
+  const [theme, setTheme] = useState("red");
   return (
-    <div className="App">
-      {/* <Counter initialCount={0} /> */}
-      <CounterHooks initialCount={0} />
-    </div>
+    <ThemeContext.Provider value={{ background: theme }}>
+      <div className="App">
+        Counter with Class
+        <Counter initialCount={0} />
+        Counter with Hooks
+        <CounterHooks initialCount={0} />
+        <button
+          onClick={() =>
+            setTheme((prevTheme) => {
+              return prevTheme === "red" ? "blue" : "red";
+            })
+          }
+        >
+          Toggle Theme
+        </button>
+      </div>
+    </ThemeContext.Provider>
   );
 }
--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+
+import { ThemeContext } from "./App";
 
 export default function CounterHooks({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const style = useContext(ThemeContext);
   return (
     <>
-      <button onClick={() => setCount((prevState) => prevState - 1)}>-</button>
+      <button
+        style={style}
+        onClick={() => setCount((prevState) => prevState - 1)}
+      >
+        -
+      </button>
       <span>{count}</span>
-      <button onClick={() => setCount((prevState) => prevState + 1)}>+</button>
+      <button
+        style={style}
+        onClick={() => setCount((prevState) => prevState + 1)}
+      >
+        +
+      </button>
     </>
   );
 }
